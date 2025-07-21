51b988a44cac1683059dbf518da7c5a5c885895f
fahim118877 <bestbot0110@gmail.com>
2025-07-21 22:09:22 +0600

    Update 0sub.js

--- Priyansh/commands/0sub.js
+++ Priyansh/commands/0sub.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("Priyansh rajput")==0 || event.body.indexOf("sub")==0 || event.body.indexOf("subscribe")==0 || event.body.indexOf("Priyansh")==0) {
 		var msg = {
-				body: "👋For Any Kind Of Help Contact On Telegram  Username 👉 @Priyanshrajput😇",
+				body: "👋For Any Kind Of Help Contact On Telegram  Username 👉 @saiko_fahim😇",
 				attachment: fs.createReadStream(__dirname + `/noprefix/sub.mp3`)
 			}
 			api.sendMessage(msg, threadID, messageID);
